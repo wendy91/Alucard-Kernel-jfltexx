f21282e70e81396b1584fc0ba9a47fef6314f6ea
alucard24 <dmaboh2@gmail.com>
2013-10-11 00:49:51 +0200

    Revert "Drivers changes"
    This reverts commit d1d284ce26ce7a3ef57d32b3ea67b9e920698c2a.

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -899,12 +899,7 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 	struct input_dev *input;
 	int i, error;
 	int wakeup = 0;
-<<<<<<< HEAD
 	int ret=0;
-=======
-#ifdef CONFIG_SENSORS_HALL
-	int ret = 0;
->>>>>>> a6f7d38... Drivers changes
 	struct device *sec_key;
 
 	if (!pdata) {
